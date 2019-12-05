d8d2f786eb96beef3bdb2892cdd253742b9977c0
esdeath11 <esdeath11@users.noreply.github.com>
2019-12-05 07:24:37 +0700

    update

--- src/Equipment.java
+++ src/Equipment.java
@@ -8,11 +8,11 @@ public class Equipment{
 class Weapon extends Equipment{
     public String wpName[] = {"Knive","Dagger","Sword","Axe","Legendary Sword"};
     public int Cost[] = {100,300,400,600,2000};
-    public int Tier[] = getWpTier();
+ //   public int Tier[] = getWpTier();
 }
 class Armor extends Equipment{
     public String armorName[] = {"Cloth","Leather armor","iron Armor","Steel Armor","Legendary Armor"};
-    public int Tier[] = getWpTier();
+ //   public int Tier[] = getWpTier();
     public int armorCost[]={150,300,600,700,2000};
-    public int ArmorHp[] = {20,30,45,50,100};
+  //  public int ArmorHp[] = {20,30,45,50,100};
 }
